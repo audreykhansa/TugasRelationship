f7f26801604636dd63852c296769a4baeee0333e
audreykhansa <audreykhsalra@gmail.com>
2025-03-10 13:18:19 +0700

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,19 @@
+package Tugas3_AudreyKhansaLarasati;
+
+class Customer {
+    private String name;
+    private String phoneNumber;
+
+    public Customer(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
